988bff0586f9551c8aee437dbd41d08c967c4694
Austin Suarez <austinmsuarez@gmail.com>
2022-04-02 12:16:54 -0500

    Initial Commit

---
+++ Drawing Paths and Shapes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Drawing Paths and Shapes
+//
+//  Created by Austin Suarez on 4/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Drawing Paths and Shapes/Drawing_Paths_and_ShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drawing_Paths_and_ShapesApp.swift
+//  Drawing Paths and Shapes
+//
+//  Created by Austin Suarez on 4/2/22.
+//
+
+import SwiftUI
+
+@main
+struct Drawing_Paths_and_ShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
